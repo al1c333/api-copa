c8257f6b540ef8c9022631a05aa0d8b9c4f362e8
al1c333 <161310536+al1c333@users.noreply.github.com>
2024-09-08 16:55:07 -0300

    Add files via upload

---
+++ questao5.js
@@ -0,0 +1,22 @@
+async function exibir_dados (){
+    try {
+        // fazendo requisição para a API
+        const response = await fetch("https://worldcupjson.net/matches/64")
+        const final_data = await response.json()
+        const mainId = document.getElementById("conteudo")
+        mainId.innerHTML = `
+            <div class="time-info">
+                <h3>Campeão: ${final_data.home_team.name}</h3>
+                <p><b>${final_data.home_team.name}</b> ${final_data.home_team.goals} x ${final_data.away_team.goals} <b>${final_data.away_team.name}</b></p>
+            </div>
+            <div class="detalhes">
+                <p><strong>Local e Estádio:</strong> ${final_data.location} - ${final_data.venue}</p>
+                <p><strong>Data e Hora:</strong> ${new Date(final_data.datetime).toLocaleString()}</p>
+            </div>
+        `
+    } catch (error){
+        console.error("Erro ao obter dados", error)
+    }
+    
+}
+exibir_dados()
